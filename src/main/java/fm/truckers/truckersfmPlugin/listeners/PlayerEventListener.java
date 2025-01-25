f54ecd4362d451239d7f9926b3f5cf022766496d
Diego <me@diegor.nl>
2025-01-25 17:54:24 +0100

    Hide advancement announcements from hidden players

--- src/main/java/fm/truckers/truckersfmPlugin/listeners/PlayerEventListener.java
+++ src/main/java/fm/truckers/truckersfmPlugin/listeners/PlayerEventListener.java
@@ -2,9 +2,14 @@ package fm.truckers.truckersfmPlugin.listeners;
 
 import com.earth2me.essentials.Essentials;
 import fm.truckers.truckersfmPlugin.TruckersfmPlugin;
+import io.papermc.paper.advancement.AdvancementDisplay;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
+import org.bukkit.advancement.Advancement;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerAdvancementDoneEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
@@ -37,6 +42,28 @@ public class PlayerEventListener implements Listener {
     public void onPlayerQuit(PlayerQuitEvent event) {
         if (hiddenPlayers.contains(event.getPlayer().getUniqueId().toString())) {
             event.quitMessage(null); // Hide the message
+        }
+    }
+
+    @EventHandler
+    public void onPlayerAdvancementDone(PlayerAdvancementDoneEvent event) {
+        if (hiddenPlayers.contains(event.getPlayer().getUniqueId().toString())) {
+            event.message(null); // Hide the message
+        }
+
+        // Get the advancement name (display title)
+        Advancement advancement = event.getAdvancement();
+        AdvancementDisplay display = advancement.getDisplay();
+
+        if (display != null) {
+            Component advancementName = display.title(); // Fetch the display title
+
+            // Send a custom message to the player
+            event.getPlayer().sendMessage(
+                    Component.text("Your advancement ", NamedTextColor.YELLOW)
+                            .append(advancementName.color(NamedTextColor.GREEN)) // Display title in green
+                            .append(Component.text(" was unlocked but not announced in chat, because your player messages are hidden.", NamedTextColor.YELLOW))
+            );
         }
     }
 }
